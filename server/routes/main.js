dcb283a912ef4fdcefe3cc966212da7de838c928
Krzysztof Druciarek <kkd26@cam.ac.uk>
2021-01-15 22:50:24 +0100

    Fix index.html path

--- server/routes/main.js
+++ server/routes/main.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+  res.sendFile(path.join(__dirname, '../../dist', 'index.html'));
 });
 
 module.exports = router;
